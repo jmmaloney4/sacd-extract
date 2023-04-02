146d53dc0ac53c09145d2f650c7ba5ac1caa9b88
Jack Maloney <jack@maloney.ai>
2023-04-02 15:48:14 -0500

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,9 +6,9 @@
     let
       supportedSystems = [ 
         "x86_64-linux"
-        # "x86_64-darwin"
         "aarch64-linux"
-        "aarch64-darwin"
+        # "x86_64-darwin"
+        # "aarch64-darwin"
       ];
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       # forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
